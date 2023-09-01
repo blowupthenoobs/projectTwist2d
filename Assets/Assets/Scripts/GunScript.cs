742a416bb91fcb7966003ee82ae9a1983c1f9681
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2023-09-01 14:52:53 -0400

    Gun shoots bullets
    but bullets have no code, have fun painting!

--- Assets/Assets/Scripts/GunScript.cs
+++ Assets/Assets/Scripts/GunScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    public GameObject bullet;
+    public GameObject firingPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,28 @@ public class GunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FaceMouse();
+        CheckInputs();
+    }
+
+    private void CheckInputs()
+    {
+        if(Input.GetAxisRaw("Fire1")!=0)
+            Fire();
+    }
+
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(bullet, firingPosition.transform.position, Quaternion.identity);
+    }
+
+    private void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition=Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = direction;
     }
 }
